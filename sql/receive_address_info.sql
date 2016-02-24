6141dba5ba9af375a2ec87b8933bd9f8a09bcb34
dakar24 <dan.lv8624@gmail.com>
2016-02-25 01:13:16 +0800

    sql

---
+++ sql/receive_address_info.sql
@@ -0,0 +1,18 @@
+/*---- create table receive_address_info---------*/
+/*---- 收货地址表 ----*/
+
+CREATE table receive_address_info (
+	address_id varchar(64)  not null  comment '地址ID',
+	user_id varchar(64)  not null  comment '用户ID',
+	receiver varchar(64)  not null  comment '收件人',
+	tel varchar(64)    comment '联系电话',
+	section varchar(256)  comment '所在地区',
+	street varchar(256)  comment '所在街区',
+	detail varchar(256) comment '详细地址',
+	post_code varchar(16) comment '邮编',
+	is_default bigint comment '是否默认',
+	gmt_create timestamp  null  comment '创建时间',
+	gmt_modified timestamp  null  comment '修改时间',
+	PRIMARY KEY (address_id),
+	INDEX  user_id_index (user_id)
+) comment='收货地址表';
