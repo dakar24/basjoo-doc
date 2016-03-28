6231728c44e0f4c0b324ec7c33953d5e649f1622
taiyuyifan <taiyuyifan@sina.com>
2016-03-29 01:23:14 +0800

    sql
    sql

--- sql/product_image_param.sql
+++ sql/product_image_param.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `product_image` (
+CREATE TABLE `product_image_param` (
   `product_id` varchar(36) NOT NULL DEFAULT '0' COMMENT '产品ID',
   `image_url` varchar(255) DEFAULT NULL COMMENT '图片地址',
   `gmt_create` timestamp NULL DEFAULT NULL COMMENT '创建时间',
   `gmt_modify` timestamp NULL DEFAULT NULL COMMENT '修改时间',
-  KEY `index_product_image_product_id` (`product_id`)
+  KEY `index_product_image_param_product_id` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='产品图片列表';
